926cca17e18f62f1e6d8014f6fc8c55e56ead6ac
Satyam Rajendra Brahmankar <satyamwani9@gmail.com>
2025-02-24 22:28:28 +0530

    refactored code version1/ authentication

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
 
-
-require('dotenv').config();
-require('./Models/db');
+require("dotenv").config();
+require("./Config/config");
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Running server');
+app.get("/", (req, res) => {
+  res.send("Running server");
 });
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/auth', AuthRouter);
-
+app.use("/auth", AuthRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
