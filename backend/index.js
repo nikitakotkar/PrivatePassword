6f5f43acc27f7eaf088dcbdbc31eb2f02f496179
nikita kotkar <nikitakotkar3703@gmail.com>
2025-02-24 21:56:18 +0530

    backend login signup validation

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = 5000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
 
-// Middleware to parse JSON
-app.use(express.json());
 
-// Sample API Route
-app.get("/", (req, res) => {
-    res.send("Welcome to the Node.js Backend!");
+require('dotenv').config();
+require('./Models/db');
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Running server');
 });
 
-// Start the Server
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on ${PORT}`)
+})
